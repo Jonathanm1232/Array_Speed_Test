e524c3fe90b0848f3d9e392ab3572e8df1d78af3
Jonathan Meineke <jonathan.meineke@conlance.de>
2023-10-18 10:39:09 +0200

    Updating main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,19 @@ import org.apache.commons.lang3.time.StopWatch;
 
 import static org.example.ArrayGenerator.ArrayGenerator;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    private List<Integer> numbers = new ArrayList<Integer>();
     static Object[] numbersArr;
     public static void main(String[] args) {
+        // Getting the Size of the Array and how many should be put in another Array
         int ammount = Integer.parseInt(JOptionPane.showInputDialog("How many numbers do you want?"));
         int outputno = Integer.parseInt(JOptionPane.showInputDialog("How many numbers do you want?"));
 
         Object[] testData = ArrayGenerator(ammount);
+
         System.out.println("RandArray Größe: " + ammount);
         System.out.println("Erstellte Größe: " + outputno);
 
+        // Starting the Test
         TimeArray(testData, outputno);
         TimeArrayList(testData, outputno);
         TimeHashtable(testData, outputno);
@@ -26,11 +26,14 @@ public class Main {
     public static void TimeArray(Object[] data, int n) {
         StopWatch stopWatch = new StopWatch();
         Object[] arr = new Object[n+1];
+
         stopWatch.start();
+
         for (int i = 0; i <= n;) {
             arr[i] = data[(int) (n * Math.random())];
             i++;
         }
+
         stopWatch.stop();
         System.out.println("Array: " + stopWatch.getNanoTime());
         stopWatch.reset();
@@ -38,12 +41,16 @@ public class Main {
     public static void TimeArrayList(Object[] data, int n) {
         StopWatch stopWatch = new StopWatch();
         ArrayList<Object> arrlist = new ArrayList<Object>();
+
         stopWatch.start();
+
         for (int i = 0; i <= n;) {
             arrlist.add(data[(int) (n * Math.random())]);
             i++;
         }
+
         stopWatch.stop();
+
         System.out.println("ArrayList: " + stopWatch.getNanoTime());
         stopWatch.reset();
     }
@@ -51,12 +58,16 @@ public class Main {
     public static void TimeHashtable(Object[] data, int n) {
         StopWatch stopWatch = new StopWatch();
         Hashtable<Integer, Object> hshtable = new Hashtable<Integer, Object>();
+
         stopWatch.start();
+
         for (int i = 0; i <= n;) {
             hshtable.put(i, data[(int) (n * Math.random())]);
             i++;
         }
+
         stopWatch.stop();
+
         System.out.println("HashTable: " + stopWatch.getNanoTime());
     }
 }
